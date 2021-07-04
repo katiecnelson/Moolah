8d76acd0488b2d39b4ea3488b91b9abbb5630b4e
Katie Nelson <katiechristine1989@gmail.com>
2021-07-04 22:16:11 +0100

    Added a bottom navigation and custom icons

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./app/navigation/TabNavigator"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+let customFonts = {
+  "Nunito-Black": require("./assets/fonts/Nunito-Black.ttf"),
+  "Nunito-Bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  "Nunito-Regular": require("./assets/fonts/Nunito-Regular.ttf"),
+  "Moolah-Icons": require("./assets/fonts/icomoon.ttf"),
+};
+
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (this.state.fontsLoaded) {
+      return (
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>
+      );
+    } else {
+      return <AppLoading />;
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/navigation/TabNavigator.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Dashboard from "../components/Dashboard";
+import Needs from "../components/Needs";
+import Wants from "../components/Wants";
+import Goals from "../components/Goals";
+import NewTransaction from "../components/NewTransaction";
+import Icon from "../components/Icon";
+
+
+const Tab = createBottomTabNavigator()
+const TabNavigator = () => {
+    return <Tab.Navigator
+        tabBarOptions={{
+            showLabel: false,
+            activeTintColor: "#48cae4",
+            inactiveTintColor: "#b7b7b7"
+        }}
+    >
+        <Tab.Screen
+            name="Dash"
+            component={Dashboard}
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="dashboard" color={color} size={size} />
+                ),
+            }}
+        />
+        <Tab.Screen
+            name="Needs"
+            component={Needs}
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="needs" color={color} size={size} />
+                ),
+            }}
+        />
+        <Tab.Screen
+            name="Wants"
+            component={Wants}
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="wants" color={color} size={size} />
+                ),
+            }}
+        />
+        <Tab.Screen
+            name="Goals"
+            component={Goals}
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="goals" color={color} size={size} />
+                ),
+            }}
+        />
+        <Tab.Screen
+            name="New"
+            component={NewTransaction}
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="new" color={color} size={size} />
+                ),
+            }}
+        />
+    </Tab.Navigator>
+}
+
+export default TabNavigator;
