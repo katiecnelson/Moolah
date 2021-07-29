bd3f4e7b95a1878bfa4d123bbbd1c19d27d54d50
Katie Nelson <katiechristine1989@gmail.com>
2021-07-29 16:53:38 +0100

    Refactor needs/wants/goals for DRY and fix pencils

--- src/screens/Goals.js
+++ src/screens/Goals.js
@@ -1,12 +1,10 @@
 import React, {useContext} from "react";
-import { View, StyleSheet, FlatList } from "react-native";
 import NeedWantGoalHeader from "../components/NeedWantGoalHeader";
-import NeedWantGoalFooter from "../components/NeedWantGoalFooter";
-import TransactionListDetail from "../components/TransactionListDetail";
 import {Context as TransactionContext} from "../context/TransactionContext";
 import {Context as CategoryIncomeContext} from "../context/CategoryIncomeContext";
-import {formatFullDate, formatAmountString, getCurrentMonth} from "../utilities/helper"
+import {getCurrentMonth} from "../utilities/helper"
 import { TabActions, useNavigation} from '@react-navigation/native';
+import NeedWantGoalPages from "../components/NeedWantGoalPages";
 
 const Goals = () => {
     const transactions = useContext(TransactionContext)
@@ -15,55 +13,20 @@ const Goals = () => {
     const currentMonth = getCurrentMonth();
     const goals = transactions.state.filter(transaction => transaction["CategoryValue"] === "three" && transaction["Date"].substring(0,7) === currentMonth);
 
-    // useEffect(() => {
-    //     //below is just for testing
-    //     transactions.getTransactions();
-    //   }, []);
-
     return (
-        <View style={styles.container}>
-            <View style={{width: "94%", height: "100%"}} >
-                <FlatList
-                    data={goals.sort((a, b) => b["Date"].localeCompare(a["Date"]))}
-                    ListHeaderComponent={() => <NeedWantGoalHeader 
-                        remaining={categoryIncome.state.remainingThree}
-                        spent={categoryIncome.state.spentThree}
-                        total={categoryIncome.state.toSpendThree}
-                        percent={categoryIncome.state.percentSpentThree}
-                        barColor="#024f86"
-                        onPress={() => navigation.dispatch(TabActions.jumpTo("New"))}
-                    />
-                    }
-                    ListFooterComponent={() => <NeedWantGoalFooter 
-                        onPress={() => navigation.navigate("History")}
-                    />
-                    }
-                    keyExtractor={(item, index) => item.ID.toString()}
-
-                    renderItem={({ item, index })=>(
-                        <View style={{backgroundColor: index % 2 === 0 ? "#efefef" : "white", borderRadius: 5}}>
-                        <TransactionListDetail
-                            key={item["ID"]}
-                            date={formatFullDate(item["Date"])}
-                            description={item["Description"] === null ? "No description" : item["Description"]}
-                            amount={formatAmountString(item["Amount"])}
-                            category={item["CategoryLabel"]}
-                            tag={item["Tag"]}
-                        />
-                        </View>
-                    )}
+        <NeedWantGoalPages 
+            data={goals.sort((a, b) => b["Date"].localeCompare(a["Date"]))}
+            header={() => <NeedWantGoalHeader 
+                remaining={categoryIncome.state.remainingThree}
+                spent={categoryIncome.state.spentThree}
+                total={categoryIncome.state.toSpendThree}
+                percent={categoryIncome.state.percentSpentThree}
+                barColor="#024f86"
+                onPress={() => navigation.dispatch(TabActions.jumpTo("New"))}
                 />
-            </View>
-        </View>
+            }
+        />
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        backgroundColor: "#ffffff",
-        flex: 1,
-    },
-});
 
 export default Goals;
--- src/screens/Wants.js
+++ src/screens/Wants.js
@@ -1,12 +1,10 @@
-import React, {useContext, useEffect} from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import React, {useContext} from "react";
 import NeedWantGoalHeader from "../components/NeedWantGoalHeader";
-import NeedWantGoalFooter from "../components/NeedWantGoalFooter";
-import TransactionListDetail from "../components/TransactionListDetail";
 import {Context as TransactionContext} from "../context/TransactionContext";
 import {Context as CategoryIncomeContext} from "../context/CategoryIncomeContext";
-import {formatFullDate, formatAmountString, getCurrentMonth} from "../utilities/helper"
+import {getCurrentMonth} from "../utilities/helper"
 import { TabActions, useNavigation} from '@react-navigation/native';
+import NeedWantGoalPages from "../components/NeedWantGoalPages";
 
 const Wants = () => {
     const transactions = useContext(TransactionContext)
@@ -15,51 +13,20 @@ const Wants = () => {
     const currentMonth = getCurrentMonth();
     const wants = transactions.state.filter(transaction => transaction["CategoryValue"] === "two" && transaction["Date"].substring(0,7) === currentMonth);
 
-      return (
-        <View style={styles.container}>
-            <View style={{width: "94%", height: "100%"}} >
-                <FlatList
-                    data={wants.sort((a, b) => b["Date"].localeCompare(a["Date"]))}
-                    ListHeaderComponent={() => <NeedWantGoalHeader 
-                        // title={categoryIncome.state.nameTwo}
-                        remaining={categoryIncome.state.remainingTwo}
-                        spent={categoryIncome.state.spentTwo}
-                        total={categoryIncome.state.toSpendTwo}
-                        percent={categoryIncome.state.percentSpentTwo}
-                        barColor="#1489cc"
-                        onPress={() => navigation.dispatch(TabActions.jumpTo("New"))}
-                    />
-                    }
-                    ListFooterComponent={() => <NeedWantGoalFooter 
-                        onPress={() => navigation.navigate("History")}
-                    />
-                    }
-                    keyExtractor={(item, index) => item.ID.toString()}
-
-                    renderItem={({ item, index })=>(
-                        <View style={{backgroundColor: index % 2 === 0 ? "#efefef" : "white", borderRadius: 5}}>
-                        <TransactionListDetail
-                            key={item["ID"]}
-                            date={formatFullDate(item["Date"])}
-                            description={item["Description"] === null ? "No description" : item["Description"]}
-                            amount={formatAmountString(item["Amount"])}
-                            category={item["CategoryLabel"]}
-                            tag={item["Tag"]}
-                        />
-                        </View>
-                    )}
+    return (
+        <NeedWantGoalPages 
+            data={wants.sort((a, b) => b["Date"].localeCompare(a["Date"]))}
+            header={() => <NeedWantGoalHeader 
+                remaining={categoryIncome.state.remainingTwo}
+                spent={categoryIncome.state.spentTwo}
+                total={categoryIncome.state.toSpendTwo}
+                percent={categoryIncome.state.percentSpentTwo}
+                barColor="#024f86"
+                onPress={() => navigation.dispatch(TabActions.jumpTo("New"))}
                 />
-            </View>
-        </View>
+            }
+        />
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        backgroundColor: "#ffffff",
-        flex: 1,
-    },
-});
 
 export default Wants;
