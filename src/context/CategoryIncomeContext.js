5bc4c0074818c5b795c16815263a40a6f585a9df
Katie Nelson <katiechristine1989@gmail.com>
2021-07-29 14:24:23 +0100

    Spent values in categories can now be zero

--- src/context/CategoryIncomeContext.js
+++ src/context/CategoryIncomeContext.js
@@ -18,34 +18,39 @@ const getCategoriesIncome = dispatch => {
     const incomeCall = await database.getIncome();
     const spent = await database.getSpent();
     const income = await incomeCall[0]["Amount"];
+    const spentOne = spent.find(category => category["Category"] === 1) ? spent.find(category => category["Category"] === 1)["SUM(Amount)"]: 0;
+    const spentTwo = spent.find(category => category["Category"] === 2) ? spent.find(category => category["Category"] === 2)["SUM(Amount)"]: 0;
+    const spentThree = spent.find(category => category["Category"] === 3) ? spent.find(category => category["Category"] === 3)["SUM(Amount)"]: 0;
 
     console.log("FROM CONTEXT: get categories and income worked!")
 
     dispatch({ type: 'get_categories_income', payload: {
         income: income,
 
+        // state.find(transaction => transaction["ID"] === route.params.ID)
+
         valueOne: categories[0]["CategoryValue"],
         labelOne: categories[0]["CategoryLabel"], 
         toSpendOne: formatAmountString(getToSpend(income, categories[0]["Percent"])),
-        spentOne: formatAmountString(spent[0]["SUM(Amount)"]),
-        remainingOne: formatAmountString(calculateRemaining(getToSpend(income, categories[0]["Percent"]), spent[0]["SUM(Amount)"])),
-        percentSpentOne: percentSpent(getToSpend(income, categories[0]["Percent"]), spent[0]["SUM(Amount)"]),
+        spentOne: formatAmountString(spentOne),
+        remainingOne: formatAmountString(calculateRemaining(getToSpend(income, categories[0]["Percent"]), spentOne)),
+        percentSpentOne: percentSpent(getToSpend(income, categories[0]["Percent"]), spentOne),
         percentOne: categories[0]["Percent"], 
 
         valueTwo: categories[1]["CategoryValue"],
         labelTwo: categories[1]["CategoryLabel"],
         toSpendTwo: formatAmountString(getToSpend(income, categories[1]["Percent"])),
-        spentTwo: formatAmountString(spent[1]["SUM(Amount)"]),
-        remainingTwo: formatAmountString(calculateRemaining(getToSpend(income, categories[1]["Percent"]), spent[1]["SUM(Amount)"])),
-        percentSpentTwo: percentSpent(getToSpend(income, categories[1]["Percent"]), spent[1]["SUM(Amount)"]),
+        spentTwo: formatAmountString(spentTwo),
+        remainingTwo: formatAmountString(calculateRemaining(getToSpend(income, categories[1]["Percent"]), spentTwo)),
+        percentSpentTwo: percentSpent(getToSpend(income, categories[1]["Percent"]), spentTwo),
         percentTwo: categories[1]["Percent"],
 
         valueThree: categories[2]["CategoryValue"],
         labelThree: categories[2]["CategoryLabel"],
         toSpendThree: formatAmountString(getToSpend(income, categories[2]["Percent"])),
-        spentThree: formatAmountString(spent[2]["SUM(Amount)"]),
-        remainingThree: formatAmountString(calculateRemaining(getToSpend(income, categories[2]["Percent"]), spent[2]["SUM(Amount)"])),
-        percentSpentThree: percentSpent(getToSpend(income, categories[2]["Percent"]), spent[2]["SUM(Amount)"]),
+        spentThree: formatAmountString(spentThree),
+        remainingThree: formatAmountString(calculateRemaining(getToSpend(income, categories[2]["Percent"]), spentThree)),
+        percentSpentThree: percentSpent(getToSpend(income, categories[2]["Percent"]), spentThree),
         percentThree: categories[2]["Percent"],
     }});
 };
